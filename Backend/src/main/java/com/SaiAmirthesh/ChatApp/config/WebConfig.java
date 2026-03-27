5d297146003e26e5235ac08443a8a4338ce49f3c
SaiAmirthesh <saiamirthesh8419@gmail.com>
2026-03-28 00:04:33 +0530

    feat : Backend Deployement using docker

---
+++ Backend/src/main/java/com/SaiAmirthesh/ChatApp/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.SaiAmirthesh.ChatApp.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
+}
